0133dd2d4c151376e8f71661ea530e62f2f966c0
Kamil <kamil256@gmail.com>
2017-04-09 10:52:19 +0200

    Add business logic project

---
+++ GlogowskiBus.UI/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using GlogowskiBus.DAL.Concrete;
+using GlogowskiBus.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GlogowskiBus.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            using (GlogowskiBusContext ctx = new GlogowskiBusContext())
+            {
+                Coordinates coords = new Coordinates { Latitude = 1, Longitude = 2 };
+                ctx.Coordinates.Add(coords);
+                ctx.SaveChanges();
+            }
+            return "Hello World!";
+        }
+    }
+}
