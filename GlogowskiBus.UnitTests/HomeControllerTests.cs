c69e2d5ed9dc1c3158972ce273c4b7bb8d41e91f
Kamil <kamil256@gmail.com>
2017-04-29 07:02:08 +0200

    Create test for CreateRoute GET action in HomeController controller.

---
+++ GlogowskiBus.UnitTests/HomeControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using GlogowskiBus.BLL.Abstract;
+using GlogowskiBus.UI.Controllers;
+using GlogowskiBus.UI.Models;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace GlogowskiBus.UnitTests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private static BLL.Concrete.BusStop[] busStops = new BLL.Concrete.BusStop[]
+        {
+            new BLL.Concrete.BusStop()
+            {
+                BusNumbers = { "1", "2" },
+                Latitude = 3.4,
+                Longitude = 4.5
+            },
+            new BLL.Concrete.BusStop()
+            {
+                BusNumbers = { "1" },
+                Latitude = 5.6,
+                Longitude = 6.7
+            }
+        };
+
+        [Test]
+        public void CreateRoute_WhenGetRequestSent_ReturnsModelWithAllBusStops()
+        {
+            // Arrange
+            IBusStopService busStopService = Substitute.For<IBusStopService>();
+            busStopService.GetAllBusStops().Returns(busStops);
+
+            // Act
+            HomeController homeController = new HomeController();
+            ViewResult viewResult = homeController.CreateRoute();
+            HomeIndexViewModel model = viewResult.Model as HomeIndexViewModel;
+
+            // Assert
+            Assert.AreEqual(2, model.BusStops.Count());
+
+            Assert.AreEqual(2, model.BusStops.ElementAt(0).BusNumbers.Count());
+            Assert.AreEqual("1", model.BusStops.ElementAt(0).BusNumbers[0]);
+            Assert.AreEqual("2", model.BusStops.ElementAt(0).BusNumbers[1]);
+            Assert.AreEqual("3.4", model.BusStops.ElementAt(0).Latitude);
+            Assert.AreEqual("4.5", model.BusStops.ElementAt(0).Longitude);
+
+            Assert.AreEqual(1, model.BusStops.ElementAt(1).BusNumbers.Count());
+            Assert.AreEqual("1", model.BusStops.ElementAt(1).BusNumbers[0]);
+            Assert.AreEqual("5.6", model.BusStops.ElementAt(1).Latitude);
+            Assert.AreEqual("6.7", model.BusStops.ElementAt(1).Longitude);
+
+        }
+    }
+}
