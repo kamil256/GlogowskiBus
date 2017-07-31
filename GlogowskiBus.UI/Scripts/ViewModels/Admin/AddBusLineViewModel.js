6f18a42cb74a1c4b0b969fb4712f3be71f591c91
Kamil <kamil256@gmail.com>
2017-07-31 20:10:32 +0200

    Remove commented code in AddBusLineViewModel.js.

--- GlogowskiBus.UI/Scripts/ViewModels/Admin/AddBusLineViewModel.js
+++ GlogowskiBus.UI/Scripts/ViewModels/Admin/AddBusLineViewModel.js
@@ -90,7 +90,6 @@
             newDepartureTime.minutes(minutes);
             newDepartureTime.dayOfWeek(engine.selectedDayOfWeek());
             self.selectedRoute().departureTimes.push(newDepartureTime);
-            //self.selectedRoute().sortDepartureTimes();
         }
     }
 
